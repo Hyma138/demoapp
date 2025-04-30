fa78515e4c08532faa0482c9b42009d49782cbd3
Hyma138 <hymavathisomannagari@gmail.com>
2025-04-30 18:23:23 +0530

    removing unused variable

---
+++ test.java
@@ -0,0 +1,10 @@
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.Statement;
+class AccountsStoringApplication{
+    public static void main(String args[]) throws Exception{
+        Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/ashokitdb","root","ashokit");
+        Statement statement=connection.createStatement();
+        
+    }
+}
