657e1c09b6419bd23b2a93e1f6c281b78d25f6be
Hyma138 <hymavathisomannagari@gmail.com>
2025-04-30 18:27:27 +0530

    added variable

--- test.java
+++ test.java
@@ -5,6 +5,6 @@ class AccountsStoringApplication{
     public static void main(String args[]) throws Exception{
         Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/ashokitdb","root","ashokit");
         Statement statement=connection.createStatement();
-        
+        int j=10;
     }
 }
